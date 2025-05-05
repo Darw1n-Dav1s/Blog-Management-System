a46a36d23a857f108f1aa4246a324fd4cae0be2c
Darwin T D <darwintd2003@gmail.com>
2025-05-05 11:38:20 +0530

    added edit and delete

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
+// app.js
 const express = require("express");
 const mongoose = require("mongoose");
 
 const Blog = require("./models/blog");
 const app = express();
 
-const dbURL = " MongoDB cluster link ";  //Connect Your Mongodb cluster ( Add the link of connection here)
+const dbURL = "mongodb+srv://blog-github:mwoneorublog@blog.9oblmfg.mongodb.net/";
 
 app.set("view engine", "ejs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Connect to MongoDB
 mongoose.connect(dbURL).then(() => {
   console.log("mongo connected");
   app.listen(3000);
@@ -47,9 +47,7 @@ app.post("/blogs", (req, res) => {
   });
 
   blog.save()
-    .then((result) => {
-      res.redirect("/blogs");
-    })
+    .then(() => res.redirect("/blogs"))
     .catch((err) => {
       console.log(err);
       res.render("create", { title: "New Blog", error: "Error saving the blog." });
@@ -60,7 +58,6 @@ app.get("/blogs/create", (req, res) => {
   res.render("create", { title: "New Blog" });
 });
 
-// 🔥 New: Single blog view
 app.get("/blogs/:id", (req, res) => {
   const id = req.params.id;
   Blog.findById(id)
@@ -77,16 +74,51 @@ app.get("/blogs/:id", (req, res) => {
     });
 });
 
+app.post("/blogs/:id/delete", (req, res) => {
+  Blog.findByIdAndDelete(req.params.id)
+    .then(() => res.redirect("/blogs"))
+    .catch(err => {
+      console.log(err);
+      res.render("404", { title: "Error Deleting Blog" });
+    });
+});
+
+app.get("/blogs/:id/edit", (req, res) => {
+  Blog.findById(req.params.id)
+    .then(blog => {
+      if (blog) {
+        res.render("edit", { blog, title: "Edit Blog" });
+      } else {
+        res.render("404", { title: "Blog Not Found" });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.render("404", { title: "Error Loading Blog" });
+    });
+});
+
+app.post("/blogs/:id/update", (req, res) => {
+  Blog.findByIdAndUpdate(req.params.id, {
+    title: req.body.title,
+    snippet: req.body.snippet,
+    body: req.body.body
+  })
+    .then(() => res.redirect(`/blogs/${req.params.id}`))
+    .catch(err => {
+      console.log(err);
+      res.render("404", { title: "Error Updating Blog" });
+    });
+});
+
 app.get("/about-us", (req, res) => {
   res.redirect("/about");
 });
 
-// 404 handler
 app.use((req, res) => {
   res.render("404", { title: "404 Page" });
 });
 
-// Graceful shutdown
 process.on('SIGINT', () => {
   mongoose.connection.close(() => {
     console.log('MongoDB connection closed.');
